165db8a9a1118b73faf91374de94b9d011ae2d9b
mika <micaela.legui@gmail.com>
2020-04-01 22:36:14 -0300

    Configurado correctamente el paquete de rest countries para empezar a programar en POO

---
+++ composerTest/index.php
@@ -0,0 +1,24 @@
+<?php   
+
+require_once __DIR__ . '\vendor\autoload.php';
+
+use NNV\RestCountries;
+
+$restCountries = new RestCountries;
+
+$paises = $restCountries->all();
+
+echo json_encode($paises);
+
+// use Monolog\Logger;
+// use Monolog\Handler\StreamHandler;
+
+// // create a log channel
+// $log = new Logger('name');
+// $log->pushHandler(new StreamHandler('myLog.log', Logger::WARNING));
+
+// // add records to the log
+// $log->warning('Foo');
+// $log->error('Bar');
+
+?>
